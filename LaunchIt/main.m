5a7f4cdc7c99560b907130a2a8aa19b2f45f7890
bricooke <brian@madebyrocket.com>
2011-01-25 15:31:07 -0700

    minor bug fixes

--- LaunchIt/main.m
+++ LaunchIt/main.m
@@ -1,13 +1,27 @@
 //
 //  main.m
-//  LaunchIt
+//  Launchables
 //
-//  Created by Brian Cooke on 1/15/11.
-//  Copyright 2011 Made By Rocket, Inc.. All rights reserved.
+//  Created by Brian Cooke on 12/14/10.
+//  Copyright Made By Rocket, Inc. 2010. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#import "validatereceipt.h"
 
-int main(int argc, char *argv[]) {
-  return NSApplicationMain(argc,  (const char **) argv);
+int main(int argc, char *argv[])
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  
+	// put the example receipt on the desktop (or change that path)
+#ifdef LI_APPSTORE
+#ifndef DEBUG
+	if (!validateReceiptAtPath([NSString stringWithFormat:@"%@/../_MASReceipt/receipt", [[NSBundle mainBundle] resourcePath]]))
+		exit(173);
+#endif
+#endif
+  
+  int ret = NSApplicationMain(argc,  (const char **) argv);
+  [pool drain];
+  return ret;
 }
